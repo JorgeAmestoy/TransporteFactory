e9bc466dcbd4fce547e3d6c43102f72e5d5bee99
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-03-07 20:46:53 +0100

    carpeta javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Bicicleta"},{"p":"<Unnamed>","l":"Camion"},{"p":"<Unnamed>","l":"FactoriaTransportable"},{"p":"<Unnamed>","l":"ITransportable"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
