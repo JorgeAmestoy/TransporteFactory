eae5585250ffe38e7e679293cc35e889c3b23c2d
Jorge Amestoy <jamestoysabucedo@danielcastelao.org>
2023-03-17 08:51:24 +0100

    ACtualizo la main

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,10 @@ public class Main {
         System.out.println("El coste total es: " + tran.costeTotal(20000) + " euros");
         System.out.println(tran.tipoEmbalaje(50F, 50F, 50F, 40F));
 
+        tran = FactoriaTransportable.getProducto(FactoriaTransportable.BARCO);
+        System.out.println("El coste total es: " + tran.costeTotal(20000) + " euros");
+        System.out.println(tran.tipoEmbalaje(90F, 100F, 50F, 60F));
+
 
     }
 }
